83e94a412a36a7f91249d9f9390a37f8d782f530
Siyana Slavova <siyanaslavova@gmail.com>
2015-03-24 17:17:16 +0200

    create project

---
+++ PhotoMaps/PhotoMaps/MapView.swift
@@ -0,0 +1,9 @@
+//
+//  MapView.swift
+//  PhotoMaps
+//
+//  Created by Siyana Slavova on 3/24/15.
+//  Copyright (c) 2015 Siyana Slavova. All rights reserved.
+//
+
+import Foundation
---
+++ PhotoMaps/PhotoMaps/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  PhotoMaps
+//
+//  Created by Siyana Slavova on 3/24/15.
+//  Copyright (c) 2015 Siyana Slavova. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController : UIViewController {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var centerPinImage: UIImageView!
+    
+}
